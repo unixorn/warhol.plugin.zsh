57c76851b7f2948a0a25ff92f11986b786c35ced
Joe Block <jpb@unixorn.net>
2023-03-15 14:16:54 -0600

    Commit the `warhol_debug` function
    Helps to actually _commit_ things.
    
    Closes #29
    
    Signed-off-by: Joe Block <jpb@unixorn.net>

--- warhol.plugin.zsh
+++ warhol.plugin.zsh
@@ -9,6 +9,12 @@
 PLUGIN_BIN="$(dirname $0)/bin"
 export PATH=${PATH}:${PLUGIN_BIN}
 
+function warhol_debug() {
+  if [[ -n "$DEBUG" ]]; then
+    echo "$@"
+  fi
+}
+
 if (( $+commands[grc] )); then
   warhol_debug "found grc"
   GRC=$(which grc)
@@ -100,3 +106,5 @@ fi
 if [[ -z "$LS_COLORS" ]]; then
   export LS_COLORS="di=34;40:ln=35;40:so=32;40:pi=33;40:ex=31;40:bd=34;46:cd=34;43:su=1;;41:sg=1;;46:tw=0;42:ow=0;43:"
 fi
+
+unset -f warhol_debug
